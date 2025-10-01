cdf1463703a0fbb277115175b9b45c499f8a00d7
Silvia Larrosa Marin <slm91@alu.ua.es>
2025-10-01 13:41:44 +0200

    Terminados los ejercicios Cita.java y PruebaCitas.java de L02-cita

--- L02-Clases-Objetos/L02-Plantillas/L02-cita/src/main/java/pa/Cita.java
+++ L02-Clases-Objetos/L02-Plantillas/L02-cita/src/main/java/pa/Cita.java
@@ -1,55 +1,93 @@
 package pa;
 
 public class Cita {
-	private int inicio; //0..24
-	private int fin;  //0..24
-	private int dia_semana;  //0..6
-	
-	public Cita() {
-		this.dia_semana = 0;
-		this.inicio = 0;
-		this.fin = 1;	
-	}
-	
-	public int getInicio() {
-		return inicio;
-	}
-	
-	public int getFin() {
-		return fin;
-	}
-	
-	public int getDia() {
-		return dia_semana;
-	}
-		
-	public void setDia(int dia) {
-		dia_semana= dia;
-	}
-	
-	public void setFranjaHoraria(int inicio, int fin) {
-		this.inicio= inicio;
-		this.fin = fin;		
-	}
-	
-	private boolean hora_valida(int hora) {
-		return false;
-	}
-	
-	private boolean franja_es_valida(int inicio, int fin) {
-		return false;
-	}
-		
-	public boolean solapa_con(Cita c) {
-		return false;
-	}
-	
-	private String mostrarDia(int dia) {
-		return null;
-	}
-	
-	public void imprimir() {
-		
-	}
+    private int inicio;       // de 0 a 24
+    private int fin;          // de 0 a 24
+    private int dia_semana;   // de 0 a 6, siendo 0 = Lunes y 6 = Domingo
 
+    public Cita() {
+        this.dia_semana = 0;
+        this.inicio = 0;
+        this.fin = 1;	
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public int getFin() {
+        return fin;
+    }
+
+    public int getDia() {
+        return dia_semana;
+    }
+
+    public void setDia(int dia) {
+        // Solo actualiza si el día está entre 0 y 6
+        if(dia >= 0 && dia <= 6) {
+            this.dia_semana = dia;
+        }
+    }
+
+    public void setFranjaHoraria(int inicio, int fin) {
+        // Solo asigna si franja_es_valida lo permite
+        if(franja_es_valida(inicio, fin)) {
+            this.inicio = inicio;
+            this.fin = fin;
+        }
+    }
+
+    
+    private boolean hora_valida(int hora) {
+        // Comprueba si la hora está entre 0 y 24. Es un método privado
+    	return hora >= 0 && hora <= 24;
+    }
+
+    
+    private boolean franja_es_valida(int inicio, int fin) {
+        // Comprueba con hora_valida que las horas estén entre 0 y 24 y que la
+    	// hora de inicio sea menor que la del fin. También es privado
+    	return hora_valida(inicio) && hora_valida(fin) && inicio < fin;
+    }
+
+    
+    public boolean solapa_con(Cita c) {
+        // Devuelve "true" si el día de la cita actual (this.dia_semana) y
+    	// el día de la otra cita (c.setDia()) son el mismo día y "true"
+    	// si el inicio de la cita actual (this.inicio) está antes de que
+    	// termine la otra cita (c.getFin()) o si el fin de la cita actual
+    	// (this.fin) está después del inicio de la otra cita (c.getInicio())
+    	// Es público
+        if(this.dia_semana != c.getDia()) {
+            return false;
+        }
+        return this.inicio < c.getFin() && c.getInicio() < this.fin;
+    }
+
+    
+    private String mostrarDia(int dia) {
+        // Devuleve el nombre del día con un switch que convierte un
+    	// entero en String. Es privado
+    	switch(dia) {
+            case 0: return "Lunes";
+            case 1: return "Martes";
+            case 2: return "Miércoles";
+            case 3: return "Jueves";
+            case 4: return "Viernes";
+            case 5: return "Sábado";
+            case 6: return "Domingo";
+            default: return "Día no válido";
+        }
+    }
+
+    
+    public void imprimir() {
+    	// Muestra los datos de la cita por pantalla. Es público
+        System.out.println("Datos de la cita: ");
+        System.out.println("    -hora inicio: " + inicio);
+        System.out.println("    -hora fin: " + fin);
+        System.out.println("    -dia semana: " + mostrarDia(dia_semana));
+    }
 }
+
--- L02-Clases-Objetos/L02-Plantillas/L02-cita/src/main/java/pa/PruebaCitas.java
+++ L02-Clases-Objetos/L02-Plantillas/L02-cita/src/main/java/pa/PruebaCitas.java
@@ -3,14 +3,53 @@ package pa;
 public class PruebaCitas {
 	
 	private void mostrar_si_hay_solape_entre(Cita citaA, Cita citaB) {
-		System.out.println("Comprobación de solape pendiente");
+	    System.out.println("Comprobación de solape completada");
+	    citaA.imprimir();
+	    citaB.imprimir();
+	    System.out.print("Estas dos citas ");
+	    if(citaA.solapa_con(citaB)) {
+	        System.out.println("SOLAPAN");
+	    } else {
+	        System.out.println("NO solapan");
+	    }
+	    System.out.println();
 	}
 
 	public static void main(String[] args) {		
-		PruebaCitas prueba = new PruebaCitas();
+	    PruebaCitas prueba = new PruebaCitas();
 	
-		System.out.println("\nEjercicio pendiente de realizar\n");
-		
+	    System.out.println("\nEjercicio hecho\n");
+
+	    Cita cita1 = new Cita();
+	    cita1.setDia(3); // Jueves
+	    cita1.setFranjaHoraria(10, 12);
+
+	    Cita cita2 = new Cita();
+	    cita2.setDia(3);
+	    cita2.setFranjaHoraria(11, 13);
+
+	    Cita cita3 = new Cita();
+	    cita3.setDia(3);
+	    cita3.setFranjaHoraria(7, 12);
+
+	    Cita cita4 = new Cita();
+	    cita4.setDia(3);
+	    cita4.setFranjaHoraria(10, 11);
+
+	    Cita cita5 = new Cita();
+	    cita5.setDia(3);
+	    cita5.setFranjaHoraria(16, 18);
+
+	    Cita cita6 = new Cita();
+	    cita6.setDia(2); // Miércoles
+	    cita6.setFranjaHoraria(10, 11);
+
+	    // comprobar solapes de cita1 con el resto
+	    prueba.mostrar_si_hay_solape_entre(cita1, cita2);
+	    prueba.mostrar_si_hay_solape_entre(cita1, cita3);
+	    prueba.mostrar_si_hay_solape_entre(cita1, cita4);
+	    prueba.mostrar_si_hay_solape_entre(cita1, cita5);
+	    prueba.mostrar_si_hay_solape_entre(cita1, cita6);
 
 	}
 
