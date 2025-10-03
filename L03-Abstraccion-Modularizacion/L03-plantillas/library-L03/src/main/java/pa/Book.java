7811e53f9bec4f4b7da8ab6e88fec98b3defdf99
Silvia Larrosa Marin <slm91@alu.ua.es>
2025-10-03 16:55:45 +0200

    Hechos los diagramas de clases y objetos para laboratory-L02 y mail-system-L03

---
+++ L03-Abstraccion-Modularizacion/L03-plantillas/library-L03/src/main/java/pa/Book.java
@@ -0,0 +1,21 @@
+package pa;
+/**
+ * A class that maintains information on a book.
+ * This might form part of a larger application such
+ * as a library system, for instance.
+ *
+ * @author (Insert your name here.)
+ * @version (Insert today's date here.)
+ */
+public class Book {
+    private String author;
+    private String title;
+
+    
+    public Book(String bookAuthor, String bookTitle) {
+        author = bookAuthor;
+        title = bookTitle;
+    }
+
+    // Add the methods here ...
+}
